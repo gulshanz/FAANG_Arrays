202c2fc158243356c8ec2770590391ad2bf36e66
Gulshann <gulshan@zestiot.io>
2021-08-12 15:48:02 +0530

    Started Greatest container

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static void greatestContainer(){
+        // Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+    }
 }
